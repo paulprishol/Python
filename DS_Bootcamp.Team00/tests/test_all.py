de0116e3d91acc01cabca6cbbd80509b1f0f1c6d
Pavel <144141702+paulprishol@users.noreply.github.com>
2025-11-10 15:59:45 +0300

    Delete DS_Bootcamp.Team00 directory

--- DS_Bootcamp.Team00/tests/test_all.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from tests.test_ratings import TestMoviesr, TestUsersr, ratings, mock_movies_data, mock_ratings_data
-from tests.test_tags import TestTags, mock_tags_data, tags
-from tests.test_movies import Test_Movies, mock_movie_data, movies
-from tests.test_links import Test_Links, mock_links_data, links
-
-if __name__ == "__main__":
-    pytest.main()
