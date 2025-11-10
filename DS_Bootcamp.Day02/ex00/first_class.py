ad27dbdcfb15d0436a329ee37db897b1224c6a71
Pavel <144141702+paulprishol@users.noreply.github.com>
2025-11-10 15:59:07 +0300

    Delete DS_Bootcamp.Day02 directory

--- DS_Bootcamp.Day02/ex00/first_class.py
+++
@@ -1,9 +0,0 @@
-class Must_read():
-    with open("data.csv", "r") as file:
-        print(file.read())
-
-if __name__ == '__main__':
-    try:
-        Must_read()
-    except FileNotFoundError:
-        print("Error: File not found")
--- DS_Bootcamp.Day02/ex05/config.py
+++
@@ -1,6 +0,0 @@
-input_path = "data.csv"
-output_path = "report"
-num_of_steps = 3
-template = """We have made {} observations from tossing a coin: {} of them were tails and {} of
-them were heads. The probabilities are {}% and {}%, respectively. Our
-forecast is that in the next {} observations we will have: {} tails and {} heads."""
